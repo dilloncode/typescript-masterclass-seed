0d0a330f5597acb7cd7d4d2cf14e48bc967ae150
Mike <mikedillon@gmx.com>
2018-07-21 22:44:30 -0400

    user defined type guards

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,13 @@ class Song {
 class Playlist {
     constructor(public name: string, public songs: Song[]) { }
 }
+// item is song types input, returns boolean
+function isSong(item:any): item is Song {
+    return item instanceof Song;
+}
 
 function getItemName(item: Song | Playlist) {
-    if (item instanceof Song) {
+    if (isSong(item)) {
         return item.title;
     }
     return item.name;
