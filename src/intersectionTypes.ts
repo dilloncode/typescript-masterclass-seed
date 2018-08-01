2e196abb093c22a4885b6e1be75bf3065980caf0
Mike <mikedillon@gmx.com>
2018-07-31 20:55:27 -0400

    interseciton types

---
+++ src/intersectionTypes.ts
@@ -0,0 +1,37 @@
+interface Order {
+    id: string;
+    amount: number;
+    currency: string;
+}
+
+interface Stripe {
+    card: string;
+    cvc: string;
+}
+
+interface Paypal {
+    email: string;
+}
+
+type CheckoutCard = Order & Stripe;
+type CheckoutPaypal = Order & Paypal;
+//type CheckoutABC = Order & { name: string };
+
+const order: Order = {
+    id: 'xj28s',
+    amount: 100,
+    currency: 'USD'
+};
+
+const orderCard: CheckoutCard = {
+    ...order,
+    card: '1000 2000 3000 4000',
+    cvc: '123'
+};
+
+const orderPaypal: CheckoutPaypal = {
+    ...order,
+    email: 'abc@def.com'
+};
+
+const assigned = Object.assign({}, order, orderCard);
