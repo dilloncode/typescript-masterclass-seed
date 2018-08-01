8b0f887a1dbc6b1ebc932ef17439c70f15dd7495
Mike <mikedillon@gmx.com>
2018-07-31 21:08:36 -0400

    interfaces vs type aliases

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,21 @@
-interface Order {
-    id: string;
-    amount: number;
-    currency: string;
+interface Item {
+    name: string;
 }
 
-interface Stripe {
-    type: 'stripe';
-    card: string;
-    cvc: string;
+interface Artist extends Item {
+    songs: number;
 }
 
-interface Paypal {
-    type: 'paypal';
-    email: string;
+interface Artist {
+    getSongs(): number;
 }
 
-type CheckoutCard = Order & Stripe;
-type CheckoutPaypal = Order & Paypal;
+type Artist2 = { name: string; } & Item;
 
-const order: Order = {
-    id: 'xj28s',
-    amount: 100,
-    currency: 'USD'
-};
-
-const orderCard: CheckoutCard = {
-    ...order,
-    type: 'stripe',
-    card: '1000 2000 3000 4000',
-    cvc: '123'
-};
-
-const orderPaypal: CheckoutPaypal = {
-    ...order,
-    type: 'paypal',
-    email: 'abc@def.com'
-};
-
-type Payload = CheckoutCard | CheckoutPaypal;
-
-function checkout(payload: Payload) {
-    if  (payload.type === 'stripe') {
-        console.log(payload.card, payload.cvc);
-    }
-    if (payload.type === 'paypal') {
-        console.log(payload.email);
+const newArtist: Artist = {
+    name: 'ABC',
+    songs: 5,
+    getSongs() {
+        return this.songs;
     }
 }
